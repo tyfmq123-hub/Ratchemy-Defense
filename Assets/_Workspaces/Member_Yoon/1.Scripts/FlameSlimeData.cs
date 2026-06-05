3a1f8e81c8a94ba0568003143e7a5530905c0227
sinsnghwan <sinsnghwan@naver.com>
2026-06-05 12:51:53 +0900

    Revert "Merge develop into feature/UnitsSpawn - 충돌 3개 파일 해결 (Debug.Log 제거 유지)"
    This reverts commit 54eac4bfb7bb95c1b780a03287ce8095499f4f89, reversing
    changes made to 67cffbbab564b15da5eaadc94206db35bcca893b.

--- Assets/_Workspaces/Member_Yoon/1.Scripts/FlameSlimeData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-// 화염슬라임 1단계 전용 데이터
-[CreateAssetMenu(fileName = "FlameSlimeData_T1", menuName = "EnemyUnit/FlameSlimeData_T1")]
-public class FlameSlimeData : EnemyUnitData
-{
-}
